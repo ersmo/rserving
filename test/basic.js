1541a032ea2c4e1a0a8560ab81fbb7d34df3f52c
komeon <397931692@qq.com>
2013-11-06 00:25:57 +0800

    add testMaxCollection

--- test/basic.js
+++ test/basic.js
@@ -5,33 +5,43 @@ var rserving = require('../lib/rserving').load(__dirname);
 // rio.enableRecordMode(true, {fileName: 'dump.bin'});
 // rio.enablePlaybackMode(true, {fileName: 'dump.bin'});
 rserving.maxConnection = 17
-// rserving.testMaxConnection(20, function(i) {
-//   console.log('Rserve MaxConnection is ' + i)
-// });
+
 function show (index, start_time) {
     return function(err, res) {
         console.log(err, index, (Date.now() - start_time)/1000);
     };
 }
 
-  // rserving.on('in', function(count) {
-  //   console.log('..........in now ' + count)
-  // })
+  rserving.on('in', function(count) {
+    console.log('..........in now ' + count)
+  })
   rserving.on('out', function(count) {
     console.log('..........out now ' + count)
   })  
 
+var args = {
+    prods: ["IBM", "YHOO", "MSFT"]
+};
+
 var i = -1;
 
 var tasks = [];
-while (++i < 50) {
+while (++i < 100) {
   tasks.push({
-    name: 'ex1',
+    name: 'ex2',
     options: {
+      entryPoint: "getOptimalPortfolio",
+      data: args,      
       callback: show(i, Date.now())
     }
   });
 
 }
 
-rserving.queue.push(tasks);
+// rserving.setOption({
+//   host: '127.0.0.1',
+//   port: 6311,
+//   user: null
+// });
+// rserving.queue.push(tasks);
+rserving.testMaxConnection()
